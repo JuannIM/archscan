6610b2a8c0c065403ce87b45dc019fe88ff2e838
JuannIM <shanys.mora@gmail.com>
2026-09-02 17:42:10 -0300

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+	"gopkg.in/yaml.v3"
+)
+
+type ArchscanConfig struct {
+	Exclude    []string   `yaml:"exclude"`
+	Thresholds Thresholds `yaml:"thresholds"`
+	Rules      Rules      `yaml:"rules"`
+}
+
+type Thresholds struct {
+	GodFunctionLines  int `yaml:"god_function_lines"`
+	DuplicateMinLines int `yaml:"duplicate_min_lines"`
+	MaxFileViolations int `yaml:"max_file_violations"`
+}
+
+type Rules struct {
+	CheckBoundaries   bool `yaml:"check_boundaries"`
+	CheckNaming       bool `yaml:"check_naming"`
+	CheckDuplication  bool `yaml:"check_duplication"`
+	CheckAntipatterns bool `yaml:"check_antipatterns"`
+}
+
+func DefaultConfig() *ArchscanConfig {
+	return &ArchscanConfig{
+		Exclude: []string{},
+		Thresholds: Thresholds{
+			GodFunctionLines:  80,
+			DuplicateMinLines: 8,
+			MaxFileViolations: 20,
+		},
+		Rules: Rules{
+			CheckBoundaries:   true,
+			CheckNaming:       true,
+			CheckDuplication:  true,
+			CheckAntipatterns: true,
+		},
+	}
+}
+
+func Load(repoPath string) *ArchscanConfig {
+	cfg := DefaultConfig()
+	data, err := os.ReadFile(filepath.Join(repoPath, "archscan.yaml"))
+	if err != nil {
+		return cfg
+	}
+	_ = yaml.Unmarshal(data, cfg)
+	return cfg
+}
